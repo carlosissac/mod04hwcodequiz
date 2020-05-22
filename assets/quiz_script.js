dccccd5559b873baeb8e8e0adacd2d4685b15287
Issac Angulo <issac.angulo@gmail.com>
2020-05-21 21:12:00 -0700

    validator fixes

---
+++ assets/quiz_script.js
@@ -0,0 +1,58 @@
+//var playerNameLbl = document.querySelector("#quiz-card-title");
+//var playerScoreLbl = document.querySelector("#quiz-card-subtitle");
+//var startBtn = document.querySelector("#b-start");
+//var quizList = document.querySelector("#quiz-list-group");
+//var quizQuitArea = document.querySelector("#quiz-quit-area");
+//var quitBtn = document.querySelector("#btn-quit");
+
+//alert(startBtn);
+
+
+//startBtn.addEventListener("click", clickStartBtn);
+
+
+//startBtn.addEventListener("click", clickStartBtn);
+
+//startBtn.addEventListener("click", clickStartBtn);
+
+/*var quiz = {
+
+
+   initializeQuiz: function() {
+    //make all UI controls appear
+
+    //turn Start to Green Color 
+
+    //Capture User Name 
+
+    //Capture Level 
+
+    //Set Timer
+
+    return 0;
+   } 
+}*/
+
+
+
+
+/*
+function clickStartBtn() {
+
+
+
+}*/
+
+/*
+
+function clickStartBtn() {
+    alert("STAAAAAARTTTTTTT");
+    var q = Object.create(quiz);
+
+    if(!q.initializeQuiz()) {
+        alert("UUUUUIIIIIII INITIALIZED");
+    }
+    
+
+
+}*/
