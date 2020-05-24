a4f8abf83a7a68aeab4ede74c82f8b713a356e58
Issac Angulo <issac.angulo@gmail.com>
2020-05-24 10:25:20 -0700

    UI Timer Completed

--- assets/quiz_script.js
+++ assets/quiz_script.js
@@ -1,5 +1,15 @@
+//////////MAIN UI ELEMENTS 
 var quizTitle = document.querySelector("#quiz-card-header");
+var startBtnContainer = document.querySelector("#btn-start-container");
 var startBtn = document.querySelector("#btn-start");
+var quizTxt1 = document.querySelector("#quiz-question-card-text-1");
+var quizTxt2 = document.querySelector("#quiz-question-card-text-2");
+var quizQuestArea = document.querySelector("#quiz-question");
+var quizList = document.querySelector("#quiz-list-group");
+var quizQuitArea = document.querySelector("#quiz-quit-area");
+var quitBtn = document.querySelector("#btn-quit");
+
+////////MODAL ELEMENTS
 var playerNameInput = document.querySelector("#quiz-modal-playername");
 var modalMsgArea = document.querySelector("#quiz-modal-msgarea");
 var btnModalLvlEasy = document.querySelector("#quiz-modal-btn-lvleasy");
@@ -7,11 +17,8 @@ var btnModalLvlNormal = document.querySelector("#quiz-modal-btn-lvlnormal");
 var btnModalLvlHard = document.querySelector("#quiz-modal-btn-lvlhard");
 var btnModalQuit = document.querySelector("#quiz-modal-btn-quit");
 var modalTimer = document.querySelector("#modal-timer");
-var quizList = document.querySelector("#quiz-list-group");
-var quizQuitArea = document.querySelector("#quiz-quit-area");
-var quitBtn = document.querySelector("#btn-quit");
 
-
+////EventListeners
 startBtn.addEventListener("click", clickStartBtn);
 quitBtn.addEventListener("click", clickQuitBtn);
 btnModalLvlEasy.addEventListener("click",clickModalEasy);
@@ -20,33 +27,63 @@ btnModalLvlHard.addEventListener("click",clickModalHard);
 btnModalQuit.addEventListener("click",clickModalQuit);
 
 var quiz = {
-
     "player_name": "",
     "quiz_level": "",
+    "quiz_score": 0,
+
+    startBtnChangeStyle: function(newstyle) {
+        if(newstyle === "quiz-start") {
+            startBtn.setAttribute("class","btn btn-success disabled btn-lg btn-block");
+            startBtn.disabled = true;
+        }
+        else if (newstyle === "initial-state") {
+            startBtn.setAttribute("class","btn btn-primary btn-lg btn-block");
+            startBtn.textContent = "START";
+            startBtn.disabled = false;
+        }
+        else {
+            console.log("startBtnChangeStyle NA");
+        }
+        return 0;
+    },
+
     showCtrlsUI: function() {
+        quiz.startBtnChangeStyle("quiz-start");
+        startBtn.textContent = "TIME";
+        quizTxt1.textContent = "";
+        quizTxt2.textContent = "";
         quizQuitArea.style.display = "block";
         quizList.style.display = "block";
     },
 
     resetCtrlsUI: function () {
+        this.startBtnChangeStyle("initial-state");
         quizTitle.textContent = "Js Quiz App";
         quizQuitArea.style.display = "none";
         quizList.style.display = "none";
+        quizTxt1.textContent = "Test your JS knowledge !!!!";
+        quizTxt2.textContent = "Click \"START\" to begin.";
     },
 
     resetModalUI: function() {
-        btnModalLvlEasy.style.display = "block";
-        btnModalLvlNormal.style.display = "block";
-        btnModalLvlHard.style.display = "block";
+        btnModalLvlEasy.setAttribute("class","btn btn-warning");
+        btnModalLvlEasy.disabled = false;
+        btnModalLvlNormal.setAttribute("class","btn btn-warning");
+        btnModalLvlEasy.disabled = false;
+        btnModalLvlHard.setAttribute("class","btn btn-warning");
+        btnModalLvlEasy.disabled = false;
         playerNameInput.value = ""; 
         modalMsgArea.value = "";
         modalMsgArea.style.backgroundColor = "white";
     },
 
     disableModalBtn: function() {
-        btnModalLvlEasy.style.display = "none";
-        btnModalLvlNormal.style.display = "none";
-        btnModalLvlHard.style.display = "none";
+        btnModalLvlEasy.setAttribute("class","btn btn-warning disabled");
+        btnModalLvlEasy.disabled = true;
+        btnModalLvlNormal.setAttribute("class","btn btn-warning disabled");
+        btnModalLvlEasy.disabled = true;
+        btnModalLvlHard.setAttribute("class","btn btn-warning disabled");
+        btnModalLvlEasy.disabled = true;
     },
 
     setLvlEasy: function () {
@@ -63,6 +100,68 @@ var quiz = {
 
     setLvlQuit: function () {
         quiz.quiz_level = "quit";
+    },
+
+    quizTimer: function() {
+        var lblmsg = modalMsgArea.value;
+        var countdown = 0;
+        var halfway = 0;
+        var lock = false;
+        var minutes = 0;
+        var seconds = 0;
+
+        if(quiz.quiz_level === "easy") {
+            //10 MIN
+            countdown = 600;
+            halfway = 300;
+            lock = true;
+        }
+        else if (quiz.quiz_level === "normal") {
+            //8 MIN
+            countdown = 480;
+            halfway = 240
+            lock = true;
+        }
+        else if (quiz.quiz_level === "hard") {
+            //5 MIN
+            countdown = 300;
+            halfway = 150;
+            lock = true;
+        }
+        else if (quiz.quiz_level === "quit") {
+            lock = false;
+            return 1;
+        }
+        else {
+            lock = false;
+            console.log("Not Applicable");
+            return 1;
+        }
+
+        if(lock) {
+            var secondInterval = setInterval(function() {
+                    if(countdown>0) {
+                        // COUNTDOWN
+                        minutes = Math.floor(countdown/60);
+                        seconds = Math.floor(countdown%60);
+                        startBtn.textContent = minutes + " : " + seconds;
+                    }
+                    if(countdown === halfway) {
+                        //HALFWAY
+                        console.log("HALFWAYYYYYYYYY");
+                    }
+                    if((countdown === 0)|| (quiz.quiz_level === "quit")) {
+                        //EXIT CONDITION
+                        startBtn.textContent = "START";
+                        clearInterval(secondInterval);
+                        return 0;
+                    }
+                    countdown--;
+            }, 1000);
+        }
+        else {
+            return 1;
+        }
     },
 
     startQuiz: function() {
@@ -89,7 +188,9 @@ var quiz = {
         }
 
         if(Boolean(start)) {
-            this.showCtrlsUI();      
+            this.showCtrlsUI();
+            this.quizTimer();
+
             return 0;
         }
         else {
@@ -113,7 +214,6 @@ var quiz = {
                         modalMsgArea.value = "QUIZ BEGINS!!!!!!";
                         modalMsgArea.style.backgroundColor = "green";
                     }
-                    
                     if((countdown === 0)|| (quiz.quiz_level === "quit")) {
                         //// TIMER EXIT CONDITION
                         quiz.resetModalUI();
@@ -221,6 +321,7 @@ function clickStartBtn(event) {
 function clickQuitBtn(event) {
     event.preventDefault();
     initialState();
+    jstest.setLvlQuit();
 }
 
 initialState();
